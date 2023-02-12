6cd8655f12e4ad0953ae3d5aa1faa2b8ac2ed8a5
klenaers <kristof@lenaers.club>
2023-02-12 19:27:06 +0100

    return to RawField

--- src/dsmr/fields.h
+++ src/dsmr/fields.h
@@ -437,7 +437,7 @@ namespace dsmr
     /*Maximum energy consumption from the current month*/
     DEFINE_FIELD(active_energy_import_maximum_demand_running_month, TimestampedFixedValue, ObisId(1, 0, 1, 6, 0), TimestampedFixedField, units::kW, units::W);
     /*Maximum energy consumption from the last 13 months*/
-    DEFINE_FIELD(active_energy_import_maximum_demand_last_13_months, String, ObisId(0, 0, 98, 1, 0), StringField, 0, 255);
+    DEFINE_FIELD(active_energy_import_maximum_demand_last_13_months, String, ObisId(0, 0, 98, 1, 0), RawField);
 
   } // namespace fields
 
